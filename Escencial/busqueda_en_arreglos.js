33621a1b957df4c2b09f66ca70ff4541d38d80bc
Edin Ramirez <22ramirez.edin@gmail.com>
2018-01-16 05:32:49 -0600

    Arreglos

---
+++ Escencial/busqueda_en_arreglos.js
@@ -0,0 +1,29 @@
+"use strict"
+
+//*****************************
+//**** Filtrar Arreglos
+
+var menu = [{
+	  nombre: 'Ceviche',
+	  precio: 20,
+	  pais: 'Perú'
+	},
+	{
+	  nombre: 'Tacos',
+	  precio: 10,
+	  pais: 'México'
+	},
+	{
+	  nombre: 'Pasta',
+	  precio: 50,
+	  pais: 'Italia'
+	},
+	  nombre: 'Quesadilla',
+	  precio: 15,
+	  pais: 'México'
+	}
+}];
+var resultado = null;
+
+resultado = menu.find(platillo => platillo.pais  == 'México');
+console.log(resultado);
