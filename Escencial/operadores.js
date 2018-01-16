4fa956b9cf491472029144a0c46f6629dc964d15
Edin Ramirez <22ramirez.edin@gmail.com>
2018-01-16 05:00:14 -0600

    Tipos de dato y Operadores Js

---
+++ Escencial/operadores.js
@@ -0,0 +1,14 @@
+"use strict"
+
+//*****************************
+//****Operadores de asignación
+
+var datoA = 10;
+
+//Asignación simple
+var igual = datoA;
+console.log("El resultado de la asignación simple es:"+ igual);
+
+//Sumar y Asignar
+
+var datoA = 10;
