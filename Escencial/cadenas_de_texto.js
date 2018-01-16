085e342036a439ab074af4760424e469d7cedc80
Edin Ramirez <22ramirez.edin@gmail.com>
2018-01-16 05:32:38 -0600

    ccadenas de texto

---
+++ Escencial/cadenas_de_texto.js
@@ -0,0 +1,8 @@
+"use strict"
+
+//*****************************
+//**** Medir una Cadenas de Texto
+
+var mensaje = "Hola, Estoy aprendiendo Javascript";
+
+console.log("La cadena de texto tiene: " + mensaje + "letras")
