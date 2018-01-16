963c7156c7863e39461052b2ae58f0b7de0013b7
Edin Ramirez <22ramirez.edin@gmail.com>
2018-01-16 05:10:55 -0600

    Se añadieron operadores

--- Escencial/operadores.js
+++ Escencial/operadores.js
@@ -11,4 +11,26 @@ console.log("El resultado de la asignación simple es:"+ igual);
 
 //Sumar y Asignar
 
-var datoA = 10;
+var masIgual = 10;
+
+masIgual += datoA;
+//masIgual = masIgual + datoA;
+console.log("El resultado de la asignación += simple es:" + masIgual)
+
+//Restar y Asignar
+
+var menosIgual = 10;
+menosIgual += datoA;
+console.log("El resultado de la asignación -= simple es:" + menosIgual);
+
+//Multiplicar y Asignar
+
+var porIgual = 10;
+porIgual *= datoA;
+console.log("El resultado de la asignación *= simple es:" + porIgual);
+
+//Dividir y Asignar
+
+var entreIgual = 10;
+entreIgual /= datoA;
+console.log("El resultado de la asignación /= simple es:" + entreIgual);
